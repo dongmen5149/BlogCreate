555919a9b62d92f13c6df00d13508bdb34aeb447
Ryu <78216411+dongmen5149@users.noreply.github.com>
2022-01-30 14:57:58 +0900

    landing페이지 준비중

--- client/pages/Landing/index.tsx
+++ client/pages/Landing/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { Button, Container, Form, Text, Title } from "./styels";
+
+
 
 const Landing = () => {
-    return <div>ㅎㅇ</div>
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const onSubmit = useCallback(() => {
+        console.log("ㅎㅎ");
+    }, []);
+
+
+
+    return (
+        <div>
+            <h1>Review</h1>
+            <Container>
+                <h2>제목</h2>
+                <div>내용</div>
+            </Container>
+            <Form>
+                <Title>
+                    <Text>
+
+                    </Text>
+                </Title>
+            </Form>
+            <Button onClick={onSubmit}>입력
+            </Button>
+        </div>
+    )
 }
 
 export default Landing
---
+++ client/pages/Landing/styels.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.header`
+    width: 500px;
+    height: 40px;
+    margin: 10px;
+    text-align: center;
+`
+
+export const Container = styled.div`
+    margin: 0 auto;
+    width: 80%;
+    border: 1px solid #333;
+    padding: 10px 0 30px 0;
+    border-radius: 5px;
+    margin-bottom : 50px;
+    text-align: center;
+`;
+
+export const Form = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    text-align: center;
+`
+
+
+export const Text = styled.div`
+    width: 80%;
+    min-height: 500px;
+`
+export const Button = styled.button`
+    width: 200px;
+    height: 50px;
+    font-size: 20px;
+    padding: 10px;
+    border: none;
+    background: indianred;
+    border-radius: 5px;
+    cursor: pointer;    
+`
+
+
